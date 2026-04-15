e21a6af8f24f21922cff1cf7dd006f0f1b959bd2
rknayem4 <rknayem10@gmail.com>
2026-04-16 00:31:18 +0600

    added search option

--- src/Components/TimeLine/TimeLine.jsx
+++ src/Components/TimeLine/TimeLine.jsx
@@ -4,34 +4,71 @@ import TimeLineCard from "../UI/TimeLineCard";
 
 const TimeLine = () => {
   const { friend } = useContext(AppsContext);
-  const [value, setValue] = useState("");
-  const filteredData = value
-    ? friend.filter((item) => item.btn.toLowerCase() == value.toLowerCase())
-    : friend;
+  const [value, setValue] = useState("default");
+  const filteredData =
+  value === "default"
+  ? friend
+  : friend.filter((item) => item.btn.toLowerCase() == value.toLowerCase());
+
+  const [search, setSearch] = useState("");
+      const searchedData = filteredData.filter((item) =>
+  item.name.toLowerCase().includes(search.toLowerCase())
+);
+
   return (
     <div className="min-h-screen container px-3 mx-auto my-12">
       <h2 className="text-4xl font-bold">TimeLine </h2>
       <div>
-        <fieldset className="fieldset">
-          <select
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
-            defaultValue="Pick a browser"
-            className="select"
-          >
-            <option disabled={true} >Filter Timeline </option>
-            <option value="Call">Call </option>
-            <option value="Text">Text</option>
-            <option value="Video">Video</option>
-          </select>
-        </fieldset>
+        <div className="flex justify-between items-center">
+          <fieldset className="fieldset">
+            <select
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+              defaultValue="default"
+              className="select min-w-50"
+            >
+              <option value="default" selected disabled>
+                Filter Timeline{" "}
+              </option>
+              <option value="Call">Call </option>
+              <option value="Text">Text</option>
+              <option value="Video">Video</option>
+            </select>
+          </fieldset>
+
+          <div className="dropdown dropdown-center">
+            <div tabIndex={0} role="button" className="btn m-1">
+              Sort By : ⬇️
+            </div>
+            <ul
+              tabIndex="-1"
+              className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
+            >
+              <li>
+                <a>Newest </a>
+              </li>
+              <li>
+                <a>Oldest</a>
+              </li>
+            </ul>
+          </div>
+          <input
+            type="text"
+            placeholder="Search"
+            className="input input-bordered w-24 md:w-auto"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </div>
       </div>
       {filteredData.length === 0 ? (
         <div className=" h-[30vh] content-center my-4 rounded-2xl shadow  text-center mx-12">
           <p>No data Found</p>
         </div>
       ) : (
-        filteredData.map((data) => <TimeLineCard key={data.id} data={data}></TimeLineCard>)
+        searchedData.map((data) => (
+          <TimeLineCard key={data.id} data={data}></TimeLineCard>
+        ))
       )}
     </div>
   );
